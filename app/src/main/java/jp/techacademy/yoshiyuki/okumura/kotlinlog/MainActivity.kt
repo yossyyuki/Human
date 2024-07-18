649f51472934b99d408a6edd73abcafa34b19838
yossyyuki <studyingapp0716@gmail.com>
2024-07-18 20:40:08 +0900

    test

---
+++ app/src/main/java/jp/techacademy/yoshiyuki/okumura/kotlinlog/MainActivity.kt
@@ -0,0 +1,14 @@
+package jp.techacademy.yoshiyuki.okumura.kotlinlog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("kotlintest", "ログへの出力テスト")
+    }
+}
